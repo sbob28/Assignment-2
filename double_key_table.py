dca88290b196c58a19698e01bba6b02da1e31abb
sbob28 <141696884+sbob28@users.noreply.github.com>
2023-09-18 16:23:19 +1000

    task3 updated

--- double_key_table.py
+++ double_key_table.py
@@ -37,10 +37,10 @@ class DoubleKeyTable(Generic[K1, K2, V]):
 
         self.internal_tables = ArrayR() #array to store interal hash tables
 
-        self._intialize_table_sizes(sizes, internal_sizes)
+        self._initialize_table_sizes(sizes, internal_sizes)
         #to set up the sizes of the top-lvel and internal hash tables 
 
-    def _intialize_table_sizes(self, sizes, internal_sizes):
+    def _initialize_table_sizes(self, sizes, internal_sizes):
         if sizes is None:
             self.sizes = self.TABLE_SIZES
         else:
@@ -61,7 +61,7 @@ class DoubleKeyTable(Generic[K1, K2, V]):
         #with size from sizes and current table_size index
 
         self.table_size += 1 
-        #increments table_size to prepare it for next initialisation 
+        #incrementing the index for the sizes list to prepare it for next initialisation 
         # with different size from sizes list??
 
 
@@ -74,11 +74,23 @@ class DoubleKeyTable(Generic[K1, K2, V]):
         """
 
         value = 0
+        #this initialised 'value' is used to accumulate hash value 
+        #as characters from 'key' processed
+
         a = 31415
+        #value is used in hash calculation as multiplier 
+
         for char in key:
+        #creates loop that iterates over each character 
+        # in the input key.
             value = (ord(char) + a * value) % self.table_size
+        # within the loop it calculates the new value of value using the formula
+        #by adding the code point of the character to a times the current value.
             a = a * self.HASH_BASE % (self.table_size - 1)
+        #updates the value of a for the next iteration.
+        #helps in creating a different hash value for each character in the key???
         return value
+        #represents the hash value for the input key1.
 
     def hash2(self, key: K2, sub_table: LinearProbeTable[K2, V]) -> int:
         """
@@ -130,9 +142,20 @@ class DoubleKeyTable(Generic[K1, K2, V]):
         #these indices are used to access or manipulate key-value pair
         
     def _create_internal_table(self, top_level_index: int) -> None:
-        new_internal_table = LinearProbeTable(self.internal_sizes[top_level_index])
-        new_internal_table.hash = lambda k: self.hash2(k, new_internal_table)
+        # integer representing the index where internal hash table should insert 
+        # within internal_table array
+        #corresponds to top-level key
+        internal_size = self.internal_sizes[top_level_index]
+        # retrieves the size of the internal hash table based on top_level_index
+        # uses internal sizes to tell the size for specified top level key
+        # size indicates how many slots the internal hash table should have.
+        new_internal_table = LinearProbeTable(internal_size, self.hash2)
+        # new instance of the LinearProbeTable class is created as new_internal_table.
+        # internal hash table is initialized with the determined internal_size
+        # hash2 method (used for hashing the second key) is passed as the hash function for this table.
         self.internal_tables.insert_at_index(top_level_index, new_internal_table)
+        # newly created internal hash table (new_internal_table) is inserted 
+        # at the specified top_level_index within the internal_tables array.
 
     def iter_keys(self, key:K1|None=None) -> Iterator[K1|K2]:
         """
@@ -150,6 +173,11 @@ class DoubleKeyTable(Generic[K1, K2, V]):
                 internal_table = self.internal_tables[top_level_index]
                 for internal_key in internal_table.keys():
                     yield internal_key
+
+    # provides an iterator that can be used to iterate 
+    # through either all top-level keys in the hash table
+    # or keys within a specific internal hash table
+    # based on the value of the key parameter.
 
     def keys(self, key:K1|None=None) -> list[K1|K2]:
         """
@@ -175,7 +203,10 @@ class DoubleKeyTable(Generic[K1, K2, V]):
                 internal_table = self.internal_tables[top_level_index]
                 for internal_key in internal_table.keys():
                     yield self[key, internal_key]
-
+        # iterator that can be used to iterate through either all values in the hash table 
+        # or values within a specific internal hash table 
+        # associated with a top-level key based on the value of the key parameter
+    
     def values(self, key:K1|None=None) -> list[V]:
         """
         key = None: returns all values in the table.
@@ -206,6 +237,9 @@ class DoubleKeyTable(Generic[K1, K2, V]):
         top_level_index, internal_index = self._linear_probe(top_level_key, internal_key, False)
         return self.internal_tables[top_level_index][internal_index]
 
+    # retrieve the value associated with a specific key pair 
+    # and it handles error cases by raising a KeyError when the key pair is not found 
+
     def __setitem__(self, key: tuple[K1, K2], data: V) -> None:
         """
         Set an (key, value) pair in our hash table.
@@ -217,6 +251,9 @@ class DoubleKeyTable(Generic[K1, K2, V]):
 
         if len(self) > self.table_size / 2:
             self._rehash()
+    #inserts a key-value pair into the double hash table
+    # ensures that the load factor is within an acceptable range
+    # and performs rehashing when necessary 
 
     def __delitem__(self, key: tuple[K1, K2]) -> None:
         """
@@ -233,6 +270,9 @@ class DoubleKeyTable(Generic[K1, K2, V]):
 
         if all(internal_table is None for internal_table in self.internal_tables):
             self.table_size = 0
+        # ensures proper deletion of a key-value pair from the double hash table 
+        # while also managing the internal structure and table size 
+        # raises a KeyError if the specified key does not exist in the hash table.
             
     def _rehash(self) -> None:
         """
@@ -253,27 +293,47 @@ class DoubleKeyTable(Generic[K1, K2, V]):
                 for key, value in old_internal_table:
                     self[key, key] = value
 
+        # code is responsible for resizing and rehashing the double hash table 
+        # to ensure it remains efficient as it grows in size
+        # ensures that all values are correctly reinserted into the resized hash table
+        # takes into acc best and worst scenarios ???
+
     def table_size(self) -> int:
         """
         Return the current size of the table (different from the length)
         """
-        return self.table_size
+        return len(self.top.level_table)
+    # a way to retrieve the size of the top-level hash table 
+    # in the double hash table structure
+    # represents the number of available slots for storing key-value pairs 
+   
     
     def __len__(self) -> int:
         """
         Returns number of elements in the hash table
         """
         return sum(len(internal_table) if internal_table is not None else 0 for internal_table in self.internal_tables)
-
-    def __str__(self) -> str:
-        """
+    #calculates the total number of elements in the double hash table 
+    # by summing the lengths of all internal hash tables
+    # considering that some internal tables may be None
+    # provides the count of key-value pairs stored in the double hash table.
+        """run
         String representation.
 
         Not required but may be a good testing tool.
         """
+    def __str__(self) -> str:
         result = ""
         for top_level_key in self.top_level_table.keys():
             result += f"Top-level key: {top_level_key}\n"
             for internal_key in self.iter_keys(top_level_key):
                 result += f"  Internal key: {internal_key}, Value: {self[top_level_key, internal_key]}\n"
         return result
+    #generates representation of the double hash table 
+    # by iterating through its top-level 
+    # and internal keys and values 
+    # and formatting them for display
+
+
+
+
